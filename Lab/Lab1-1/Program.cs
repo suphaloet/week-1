7f63e67e6709b66fb98818bd71b2e6b4f76e1983
suphaloet <suphaloet.skp@gmail.com>
2015-08-21 17:03:05 +0700

    remove flag variable

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -71,7 +71,7 @@ namespace Lab1_1
             {
                 j = 1;
                 for (int i = 0; i != input.Length - 1; i++)
-                {
+                { 
                     // larger to smaller
                     if (input[i] < input[i + 1])
                     {
