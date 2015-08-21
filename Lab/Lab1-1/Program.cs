c003e1c30a3cfebc5735e0386808dea962a50530
suphaloet <suphaloet.skp@gmail.com>
2015-08-21 17:02:31 +0700

    Remove flag variable

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -8,6 +8,8 @@ namespace Lab1_1
 {
     class BubbleSort
     {
+        // use flag variable
+        /*
         static void Main(string[] args)
         {
             bool flag = true;
@@ -33,14 +35,61 @@ namespace Lab1_1
                         check++;
                     }
                     // smaller to larger
-  /*                  if (input[i] > input[i + 1])
+                    /*                  if (input[i] > input[i + 1])
+                                      {
+                                          int temp = input[i];
+                                          input[i] = input[i + 1];
+                                          input[i + 1] = temp;
+                                          flag = true;
+                                          check++;
+                                      }*//*
+                }
+            }
+
+            // output
+            for (int i = 0; i != input.Length; i++)
+            {
+                Console.Write(input[i]);
+                Console.Write(" ");
+            }
+            Console.WriteLine("\nAt line 24 will be executed: {0} times", check);
+            Console.ReadKey();
+        }*/
+
+        // don't use flag variable
+        static void Main(string[] args)
+        {
+            //bool flag = true;
+            // check how many times at line 24
+            int check = 0;
+            int j = 0;
+            // input
+            int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+
+            // process
+            while (j == 0)
+            {
+                j = 1;
+                for (int i = 0; i != input.Length - 1; i++)
+                {
+                    // larger to smaller
+                    if (input[i] < input[i + 1])
                     {
                         int temp = input[i];
                         input[i] = input[i + 1];
                         input[i + 1] = temp;
-                        flag = true;
+                        j = 0;
                         check++;
-                    }*/
+                    }
+                    // smaller to larger
+                    /*                  if (input[i] > input[i + 1])
+                                      {
+                                          int temp = input[i];
+                                          input[i] = input[i + 1];
+                                          input[i + 1] = temp;
+                                          flag = true;
+                                          check++;
+                                      }*/
                 }
             }
 
